6a4055391d408cd761a2d4ca393a405527482e5e
Joseph <cliffordpaul88@gmail.com>
2022-05-01 15:20:17 +0300

    2nd commit

---
+++ ex2-dfunction.py
@@ -0,0 +1,9 @@
+def double(n)I
+return 2 n     #Return twice the given number
+#call the function with the value of your choice and print its result
+print("to call this function using its name
+#x = duouble(3)
+#print(x)
+print("The program ran succesfully")
+#------------------------------------------
+print("first modification for 2nd commit")
